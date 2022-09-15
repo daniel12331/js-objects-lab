448388b1c3aadbe8ad62d50aa6583230a7be7a5c
Daniel Marko <20088708@mail.wit.ie>
2022-09-15 11:35:22 +0100

    Nested objects.

---
+++ fundamentals.js
@@ -0,0 +1,11 @@
+const aCar = {
+    owner : "Joe Bloggs",
+    car:{
+        make: "Toyota",
+        model:"Corolla",
+        cc:"1800"
+    },
+    registration : '201WD1058'
+  };
+  
+  console.log(aCar.owner + ' drives a ' + aCar.car.make)
